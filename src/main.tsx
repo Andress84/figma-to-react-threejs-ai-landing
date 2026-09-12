f6f154e45d3fae2f38ddc8c49d0a4758c2d610ee
Andrii Laimand <45905314+Andress84@users.noreply.github.com>
2026-09-12 22:30:41 +0200

    style: align typography and design tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/poppins/latin-400.css'
+import '@fontsource/poppins/latin-600.css'
 
 import App from './App'
 import './styles/global.css'
